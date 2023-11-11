9beeec513613f3ef34123b92b66739fb7a611e94
Jakub Kołacz <jakubkolacz01@gmail.com>
2023-11-11 17:19:10 +0100

    Implemented synchronization functionality.
    Changes:
    - Fully implemented service to do product synchronisation
    - Implemented Factory and AbstractFactory patterns
    - Changed classes bodies and names
    - Config cleanup
    - Implemented connection with ProductServiceApi and AvailabilityServiceApi
    - Dependencies cleanup
    - Remove every db related this (post-template leftovers)
    
    //TODO Tests

--- src/test/groovy/com/honeybadgersoftware/productworker/base/BaseIntegrationTest.groovy
+++ src/test/groovy/com/honeybadgersoftware/productworker/base/BaseIntegrationTest.groovy
@@ -5,13 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.test.context.TestPropertySource
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.spock.Testcontainers
 import spock.lang.Shared
 import spock.lang.Specification
 
-@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:application-test.properties")
 abstract class BaseIntegrationTest extends Specification {
@@ -25,8 +21,6 @@ abstract class BaseIntegrationTest extends Specification {
     @Autowired
     protected TestRestTemplate restTemplate
 
-    @Container
-    PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:15.3")
 
     def setup() {
         addressToUseForTests = 'http://localhost:' + port
